ade9bd5104439783da926c75f3f2029904995aa5
Dan Foster <fosted3@rpi.edu>
2014-05-01 23:44:13 -0400

    Added options for verbose output, resume support, finer control on data retention

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,9 @@ struct settings
 	bool dump_binary;
 	bool dump_plaintext;
 	bool overwrite_data;
+	bool keep_previous_binary;
+	bool keep_previous_text;
+	bool verbose;
 	unsigned int num_particles;
 	unsigned int num_frames;
 	double size;
@@ -209,16 +212,29 @@ bool file_exists(const char *filename)
 	return ifile;
 }
 
-void dump(unsigned int frame, std::vector<particle*> &particles, bool overwrite)
+std::string gen_filename(unsigned int frame, bool binary)
 {
-	unsigned int size = sizeof(particle);
 	std::string filename = std::to_string(frame);
 	while (filename.length() < 4)
 	{
 		filename.insert(0, "0");
 	}
-	filename += ".dat";
 	filename.insert(0, "./data/");
+	if (binary)
+	{
+		filename += ".dat";
+	}
+	else
+	{
+		filename += ".txt";
+	}
+	return filename;	
+}
+
+void dump(unsigned int frame, std::vector<particle*> &particles, bool overwrite, bool keep_prev)
+{
+	unsigned int size = sizeof(particle);
+	std::string filename = gen_filename(frame, true);
 	if (!overwrite && file_exists(filename.c_str()))
 	{
 		std::cerr << "Overwrite disabled, cannot overwrite " << filename << ". Aborting." << std::endl;
@@ -227,6 +243,13 @@ void dump(unsigned int frame, std::vector<particle*> &particles, bool overwrite)
 	if (file_exists(filename.c_str()))
 	{
 		assert(remove(filename.c_str()) == 0);
+	}
+	if (!keep_prev && frame > 0)
+	{
+		if (file_exists(gen_filename(frame - 1, true).c_str()))
+		{
+			assert(remove(gen_filename(frame - 1, true).c_str()) == 0);
+		}
 	}
 	std::fstream outfile(filename, std::ios::out | std::ios::binary);
 	outfile.seekp(0);
@@ -237,16 +260,40 @@ void dump(unsigned int frame, std::vector<particle*> &particles, bool overwrite)
 	outfile.close();
 }
 
-void dump_plaintext(unsigned int frame, std::vector<particle*> &particles, bool overwrite)
+unsigned int read_data(std::vector<particle*> &particles, quadtree *root, unsigned int num_frames)
 {
-	std::string filename = std::to_string(frame);
-	vector temp;
-	while (filename.length() < 4)
+	assert (particles.size() == 0);
+	unsigned int size = sizeof(particle);
+	unsigned int frame = num_frames;
+	unsigned int num_particles;
+	while (!file_exists(gen_filename(frame, true).c_str()) && frame > 0)
 	{
-		filename.insert(0, "0");
+		frame--;
 	}
-	filename += ".txt";
-	filename.insert(0, "./data/");
+	if (frame == 0)
+	{
+		return 0;
+	}
+	std::fstream infile(gen_filename(frame, true), std::ios::in | std::ios::binary);
+	particle temp;
+	particle *to_add;
+	infile.seekg(0, infile.end);
+	num_particles = infile.tellg() / size;
+	infile.seekg(0, infile.beg);
+	for (unsigned int i = 0; i < num_particles; i++)
+	{
+		infile.read((char*) &temp, size);
+		to_add = new particle(temp);
+		particles.push_back(to_add);
+		root -> add_particle(to_add);
+	}
+	return frame + 1;
+}
+
+void dump_plaintext(unsigned int frame, std::vector<particle*> &particles, bool overwrite, bool keep_prev)
+{
+	std::string filename = gen_filename(frame, false);
+	vector temp;
 	if (!overwrite && file_exists(filename.c_str()))
 	{
 		std::cerr << "Overwrite disabled, cannot overwrite " << filename << ". Aborting." << std::endl;
@@ -255,6 +302,13 @@ void dump_plaintext(unsigned int frame, std::vector<particle*> &particles, bool 
 	if (file_exists(filename.c_str()))
 	{
 		assert(remove(filename.c_str()) == 0);
+	}
+	if (!keep_prev && frame > 0)
+	{
+		if (file_exists(gen_filename(frame - 1, false).c_str()))
+		{
+			assert(remove(gen_filename(frame - 1, false).c_str()) == 0);
+		}
 	}
 	std::fstream outfile(filename, std::ios::out);
 	outfile.seekp(0);
@@ -348,6 +402,42 @@ void read_settings(settings &s, const char* sfile)
 					s.overwrite_data = false;
 				}
 			}
+			else if (var.compare("keep_previous_binary") == 0)
+			{
+				cfg >> var;
+				if (var.compare("true") == 0)
+				{
+					s.keep_previous_binary = true;
+				}
+				else
+				{
+					s.keep_previous_binary = false;
+				}
+			}
+			else if (var.compare("keep_previous_text") == 0)
+			{
+				cfg >> var;
+				if (var.compare("true") == 0)
+				{
+					s.keep_previous_text = true;
+				}
+				else
+				{
+					s.keep_previous_text = false;
+				}
+			}
+			else if (var.compare("verbose") == 0)
+			{
+				cfg >> var;
+				if (var.compare("true") == 0)
+				{
+					s.verbose = true;
+				}
+				else
+				{
+					s.verbose = false;
+				}
+			}
 			else if (var.compare("num_particles") == 0)
 			{
 				cfg >> s.num_particles;
@@ -409,6 +499,9 @@ void set_default(settings &s)
 	s.dump_binary = false;
 	s.dump_plaintext = true;
 	s.overwrite_data = false;
+	s.keep_previous_binary = false;
+	s.keep_previous_text = true;
+	s.verbose = false;
 	s.num_particles = 5000;
 	s.num_frames = 300;
 	s.size = 512;
@@ -423,6 +516,7 @@ void set_default(settings &s)
 
 int main(int argc, char **argv)
 {
+	unsigned int start_frame = 0;
 	settings config;
 	set_default(config);
 	if (argc == 1)
@@ -457,16 +551,32 @@ int main(int argc, char **argv)
 	struct thread_data td[NUM_THREADS];
 	int rc;
 #endif
-	std::cout << "Generating particles..." << std::endl;
-	for (unsigned int i = 0; i < config.num_particles; i++)
+	if (config.read_existing)
 	{
-		generate_particle(config, particles, root);
+		start_frame = read_data(particles, root, config.num_frames);
+		if (start_frame == 0)
+		{
+			std::cerr << "No data to resume from. Aborting." << std::endl;
+			exit(1);
+		}
+		std::cout << "Resuming from " << start_frame << std::endl;
 	}
-	for (unsigned int i = 0; i < config.num_frames; i++)
+	else
+	{
+		std::cout << "Generating particles..." << std::endl;
+		for (unsigned int i = 0; i < config.num_particles; i++)
+		{
+			generate_particle(config, particles, root);
+		}
+	}
+	for (unsigned int i = start_frame; i < config.num_frames; i++)
 	{
 		std::cout << "Frame " << i + 1 << "/" << config.num_frames << std::endl;
+		if (config.verbose) { std::cout << "Calculating masses of nodes..." << std::endl; }
 		root -> calc_mass();
+		if (config.verbose) { std::cout << "Calculating COMs of nodes..." << std::endl; }
 		root -> calc_com();
+		if (config.verbose) { std::cout << "Starting Barnes-Hut algorithm..." << std::endl; }
 #ifndef THREADED
 		barnes_hut(particles, root, config.theta, config.display_progress);
 #endif
@@ -487,9 +597,13 @@ int main(int argc, char **argv)
 			}
 		}
 #endif
+		if (config.verbose) { std::cout << "Updating particles..." << std::endl; }
 		update_all(particles, config.dt);
+		if (config.verbose) { std::cout << "Checking for strays..." << std::endl; }
 		check_tree(particles, root);
+		if (config.verbose) { std::cout << "Deallocating nodes..." << std::endl; }
 		delete root;
+		if (config.verbose) { std::cout << "Regenerating nodes..." << std::endl; }
 		root = new quadtree(&origin, config.size);
 		for (unsigned int i = 0; i < particles.size(); i++)
 		{
@@ -497,11 +611,13 @@ int main(int argc, char **argv)
 		}
 		if (config.dump_binary)
 		{
-			dump(i, particles, config.overwrite_data);
+			if (config.verbose) { std::cout << "Dumping binary data..." << std::endl; }
+			dump(i, particles, config.overwrite_data, config.keep_previous_binary);
 		}
 		if (config.dump_plaintext)
 		{
-			dump_plaintext(i, particles, config.overwrite_data);
+			if (config.verbose) { std::cout << "Dumping plaintext data..." << std::endl; }
+			dump_plaintext(i, particles, config.overwrite_data, config.keep_previous_text);
 		}
 	}
 	delete root;
