838d33b01bf94bfe91b9af47e9805013f5b72f63
Dan Foster <fosted3@rpi.edu>
2014-05-01 21:11:48 -0400

    Added psudo-transparancy support to draw

--- src/draw.py
+++ src/draw.py
@@ -17,6 +17,9 @@ def get_tuple(line):
 	temp[2] = float(temp[2])
 	return tuple(temp)
 
+def inc_tuple(tup, inc):
+	return (clamp(0, tup[0] + inc, 255), clamp(0, tup[1] + inc, 255), clamp(0, tup[2] + inc, 255))
+
 def gen_data(frame):
 	fname = str(frame)
 	while (len(fname) < 4):
@@ -31,23 +34,24 @@ def gen_image(frame):
 	fname = "img/" + fname + ".png"
 	return fname
 	
-def render(frame, size):
+def render(frame, size, inc):
 	try:
 		data = open(gen_data(frame), 'r')
 	except:
 		return False
 	im = Image.new("RGB", (size, size))
 	pix = im.load()
+	i = 0
 	for line in data:
 		temp = get_tuple(line)
 		x = clamp(0, int(temp[0] + size/2), size - 1)
 		y = clamp(0, int(temp[1] + size/2), size - 1)
-		pix[x, y] = (255, 255, 255)
+		pix[x, y] = inc_tuple(pix[x, y], inc)
 	print("Saving " + gen_image(frame))
 	im.save(gen_image(frame))
 	return True
 	
-def render_iso(frame, size):
+def render_iso(frame, size, inc):
 	if (os.path.isfile(gen_image(frame))):
 		return True
 	try:
@@ -56,16 +60,18 @@ def render_iso(frame, size):
 		return False
 	im = Image.new("RGB", (size * 2, size * 2))
 	pix = im.load()
+	i = 0
 	for line in data:
 		temp = get_tuple(line)
 		x = clamp(0, int((sqrt(3) / 2.0) * (temp[0] - temp[1]) + size), (size * 2) - 1)
 		y = clamp(0, int((-0.5) * (temp[0] + temp[1] + 2 * temp[2]) + size), (size * 2) - 1)
-		pix[x, y] = (255, 255, 255)
+		pix[x, y] = inc_tuple(pix[x, y], inc)
 	print("Saving " + gen_image(frame))
 	im.save(gen_image(frame))
 	return True
 	
 frame = 0
 size = 1024
-while (render_iso(frame, size)):
+inc = 2
+while (render_iso(frame, size, inc)):
 	frame += 1
